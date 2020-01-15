69039c3f97112b9d6d8215822082dfa24c58d990
ZeCarlos88 <a72187@alunos.uminho.pt>
2020-01-15 13:40:08 +0000

    autenticação

--- projeto/app.js
+++ projeto/app.js
@@ -2,7 +2,9 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
+var passport = require('passport')
 
 var mongoose = require ('mongoose')
 mongoose.Promise = require('bluebird'); //Adicionei este modulo pq estava a dar warning de deprecated library
@@ -28,7 +30,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
